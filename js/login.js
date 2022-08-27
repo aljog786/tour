0da11b02b38fbc3ee0254dbd4d68564fa0474fb5
greymoon <aljog786@gmail.com>
2022-08-27 21:43:48 +0530

    last commit

---
+++ js/login.js
@@ -0,0 +1,136 @@
+let email=document.getElementById("email");
+let pwd=document.getElementById("pwd");
+
+// validation
+function validate() {
+// let regexp=/^([A-Za-z0-9\.-]+)@([\w\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    
+// if(regexp.test(email.value)) {
+//     alert("invalid email");
+//         return false;
+//         }
+if (email.value=="") {
+   alert("email field cannot be empty");
+   return false;
+}
+else if (pwd.value=="") {
+   alert("password field cannot be empty");
+   return false;
+}
+else if (pwd.value.length<=6) {
+   alert("password is too short");
+   pwd.value.border="2px solid red";
+   return false;
+}
+else {
+   return true;
+}
+}
+
+var LoginModalController = {
+    tabsElementName: ".logmod__tabs li",
+    tabElementName: ".logmod__tab",
+    inputElementsName: ".logmod__form .input",
+    hidePasswordName: ".hide-password",
+    
+    inputElements: null,
+    tabsElement: null,
+    tabElement: null,
+    hidePassword: null,
+    
+    activeTab: null,
+    tabSelection: 0, // 0 - first, 1 - second
+    
+    findElements: function () {
+        var base = this;
+        
+        base.tabsElement = $(base.tabsElementName);
+        base.tabElement = $(base.tabElementName);
+        base.inputElements = $(base.inputElementsName);
+        base.hidePassword = $(base.hidePasswordName);
+        
+        return base;
+    },
+    
+    setState: function (state) {
+    	var base = this,
+            elem = null;
+        
+        if (!state) {
+            state = 0;
+        }
+        
+        if (base.tabsElement) {
+        	elem = $(base.tabsElement[state]);
+            elem.addClass("current");
+            $("." + elem.attr("data-tabtar")).addClass("show");
+        }
+  
+        return base;
+    },
+    
+    getActiveTab: function () {
+        var base = this;
+        
+        base.tabsElement.each(function (i, el) {
+           if ($(el).hasClass("current")) {
+               base.activeTab = $(el);
+           }
+        });
+        
+        return base;
+    },
+   
+    addClickEvents: function () {
+    	var base = this;
+        
+        base.hidePassword.on("click", function (e) {
+            var $this = $(this),
+                $pwInput = $this.prev("input");
+            
+            if ($pwInput.attr("type") == "password") {
+                $pwInput.attr("type", "text");
+                $this.text("Hide");
+            } else {
+                $pwInput.attr("type", "password");
+                $this.text("Show");
+            }
+        });
+ 
+        base.tabsElement.on("click", function (e) {
+            var targetTab = $(this).attr("data-tabtar");
+            
+            e.preventDefault();
+            base.activeTab.removeClass("current");
+            base.activeTab = $(this);
+            base.activeTab.addClass("current");
+            
+            base.tabElement.each(function (i, el) {
+                el = $(el);
+                el.removeClass("show");
+                if (el.hasClass(targetTab)) {
+                    el.addClass("show");
+                }
+            });
+        });
+        
+        base.inputElements.find("label").on("click", function (e) {
+           var $this = $(this),
+               $input = $this.next("input");
+            
+            $input.focus();
+        });
+        
+        return base;
+    },
+    
+    initialize: function () {
+        var base = this;
+        
+        base.findElements().setState().getActiveTab().addClickEvents();
+    }
+};
+
+$(document).ready(function() {
+    LoginModalController.initialize();
+});
